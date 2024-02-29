db31994c22828867fac8be47fdec689edd0e5a68
RITIK <ritikprofessional111@gmail.com>
2024-02-29 19:57:42 +0530

    I wrote my first ever C++ program

---
+++ Basics/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "HELLO WORLD!";
+    std::cout << " RITIK\n";
+    std::cout << "RITIK" << std::endl;
+    std::cout << "New Line" << std::endl;
+    return 0;
+}
