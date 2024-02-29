32ccfdd3197b1211724f1fa1b6059fdf843f6942
RITIK <ritikprofessional111@gmail.com>
2024-02-29 20:40:58 +0530

    Learned basic program in C++

--- Basics/main.cpp
+++ Basics/main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+#include <iostream> // it is used to include the input output library of C++ to let take inputs and give outputs from and to the terminal respectively
 
-int main(){
-    std::cout << "HELLO WORLD!";
-    std::cout << " RITIK\n";
-    std::cout << "RITIK" << std::endl;
+int main(){ // it is the main function of the C++ program within which everything is executed
+    std::cout << "HELLO WORLD!"; // std is the name space, cout is the output stream
+    std::cout << " RITIK\n"; // in order to add a new line, we need to include "\n"
+    std::cout << "RITIK" << std::endl; // we can also add new line stream "endl" to include a new line
     std::cout << "New Line" << std::endl;
-    return 0;
+    puts("HEYY"); // it can also be used to print statements. It adds a new line automatically.
+    return 0; // return 0 makes sure that the program has been executed successfully
 }
