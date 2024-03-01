53c9485bf7e546acb9bc1ad6444e2c3b05325bd2
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-03-01 21:12:44 +0530

    Understood Variables

---
+++ Basics/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1; // initialization of an integer variable named num1
+    num1 = 2; // assignment of value
+
+    int num2 = 23; // initialisation and assignment simlutaneously
+
+    cout << num1 << "\n";
+    cout << num2 << "\n";
+
+    return 0;
+}
