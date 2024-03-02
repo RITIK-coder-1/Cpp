3f847dfdfefb165d03ef5076a0c76c80fef6fc7b
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-03-02 10:42:03 +0530

    I learned Varaibles, Tokens and Reference Variables

--- Basics/variables.cpp
+++ Basics/variables.cpp
@@ -8,8 +8,22 @@ int main(){
 
     int num2 = 23; // initialisation and assignment simlutaneously
 
+    num1 = 10; // the variable can be reassigned
+
+    const int num3 = 19;
+    // num3 = 22; it can not be reassigned as it is a constant
     cout << num1 << "\n";
     cout << num2 << "\n";
+    cout << num1 + num2 << "\n";
 
+    int num4 = 43;
+    int num5 = num4;
+    int &num6 = num4; // reference variable
+    num5 = num1; // it can be reassigned to a different variable
+    cout << "num4: " << num4 << "\n"; // unchanged // 43
+    cout << "num5: " << num5 << "\n";
+    num6 = 10;
+    // num6 = num5; it can not be reassigned to a different variable
+    cout << "num4: " << num6 << "\n"; // changed // 10
     return 0;
-}
+} 
