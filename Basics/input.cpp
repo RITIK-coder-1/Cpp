e365b2a1fef7f0084525c7bcb1c442de8e8772c7
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-03-02 13:17:39 +0530

    Escape sequences

--- Basics/input.cpp
+++ Basics/input.cpp
@@ -6,7 +6,7 @@ int main(){
     int number1, number2;
 
     // cin is used to take inputs:
-    
+
     cout << "Enter your first number: " << "\n";
     cin >> number1; 
     cout << "Enter your second number: " << "\n";
@@ -14,5 +14,7 @@ int main(){
 
     cout << "Your Sum: " << number1 + number2 << "\n";
 
+    cout << "\'ritik\'" << "\n"; // prints single quote string // 'ritik
+    cout << "\"ritik\"" << "\n"; // prints double quote string // "ritik"
     return 0;
 }
