35da2c5a14a4af10e2872cc13372d7c34a469b4f
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-03-02 11:03:05 +0530

    Understood different constants

---
+++ Basics/constants.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int number = 19; // integer constant
+    const char letter = 'R'; // character constant
+    const string name = "Ritik"; // string constant
+    const bool boolean = true; // boolean constant
+    const float num = 3.14; // floating constant
+
+    cout << name; // Ritik
+    return 0;
+}
