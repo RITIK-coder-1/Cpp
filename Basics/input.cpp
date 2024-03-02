dfe99884194a67708161115a8e812aa3f6058eaf
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-03-02 12:53:51 +0530

    Understood taking inputs

---
+++ Basics/input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number1, number2;
+
+    // cin is used to take inputs:
+    
+    cout << "Enter your first number: " << "\n";
+    cin >> number1; 
+    cout << "Enter your second number: " << "\n";
+    cin >> number2;
+
+    cout << "Your Sum: " << number1 + number2 << "\n";
+
+    return 0;
+}
