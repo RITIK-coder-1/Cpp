6df7ff942cc3bae0640d4c8f8a35c18466300c70
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-06-13 00:29:55 +0530

    strings

---
+++ Basics/string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char name[10] = "Ritik"; // stores upto 10 memory bytes
+
+    char name2[] = "Ritikkk"; // stores the reequired amount of memory
+
+    char name3[30] = {'R', 'T', 'K'};
+
+    string name4 = "Ritik"; // stores 
+
+    cout << name << endl;
+    cout << name2 << endl;
+    cout << name3 << endl;
+    cout << name4 << endl;
+
+
+    return 0;
+}
