4d2c5a65e012f2b4a151b57bbc6191b3138896a3
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-06-12 22:14:00 +0530

    restarting c++

--- Basics/namespace.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std; // we can directly include namespace here.
-
-int main(){
-    cout << "Hello Ritik!" << endl; // The entities that are being provided by the standard library can be used directly
-
-    return 0;
-}
